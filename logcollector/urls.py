f2bc6af748c600a56f71ed6ef162a38e0a084d12
BlackStar255 <sch.miklos86@gmail.com>
2014-10-28 10:35:39 +0100

    second

--- logcollector/urls.py
+++ logcollector/urls.py
@@ -3,13 +3,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from logcollector import views
 
 urlpatterns = patterns('',
-    url(r'^log/$', views.LogList.as_view()),
-    url(r'^log/(?P<pk>\d+)/$', views.LogDetail.as_view()),
-    url(r'^log/(?P<ts1>\d+)/(?P<ts2>\d+)/Max(?:/(?P<dim1>\d+))?(?:/(?P<dim2>\d+))?/$', views.LogMax.as_view()),
-    url(r'^log/(?P<ts1>\d+)/(?P<ts2>\d+)/Min(?:/(?P<dim1>\d+))?(?:/(?P<dim2>\d+))?/$', views.LogMin.as_view()),
-    url(r'^log/(?P<ts1>\d+)/(?P<ts2>\d+)/Avg(?:/(?P<dim1>\d+))?(?:/(?P<dim2>\d+))?/$', views.LogAvg.as_view()),
-    url(r'^log/(?P<ts1>\d+)/(?P<ts2>\d+)/StdDev(?:/(?P<dim1>\d+))?(?:/(?P<dim2>\d+))?/$', views.LogStdDev.as_view()),
-    url(r'^log/(?P<ts1>\d+)/(?P<ts2>\d+)/(?P<n>\d+)/MvgAvg(?:/(?P<dim1>\d+))?(?:/(?P<dim2>\d+))?/$', views.LogMvgAvg.as_view())
+    url(r'^log/', views.LogFun.as_view()),
+#    url(r'^log/listall/$', views.LogList.as_view()),
+#    url(r'^log/(?P<pk>\d+)/$', views.LogDetail.as_view()),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- logcollector/views.py
+++ logcollector/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from logcollector.models import Log
 from logcollector.serializers import LogSerializer
 from django.http import Http404
@@ -6,14 +7,65 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.db.models import Max, Min, Avg, StdDev
 
-class LogList(APIView):
-    """
-    List all logs, or create a new log.
-    """
+class LogFun(APIView):
+    
     def get(self, request, format=None):
-        log = Log.objects.all()
-        serializer = LogSerializer(log, many=True)
-        return Response(serializer.data)
+        params = request.GET
+        method = None
+        if 't1' not in params.keys() or 't2' not in params.keys():
+            log = Log.objects.all().order_by('-timestamp')[:5]
+            serializer = LogSerializer(log, many=True)
+            return Response(serializer.data)
+        rng = (params['t1'],params['t2'])
+        if 'method' in params.keys():
+            method = params['method']
+        
+        #Checking for optional parameters and filter the data
+        if 'dim1' in params.keys():
+            dim1 = params['dim1']
+            data = Log.objects.filter(dim1 = dim1)
+        if 'dim2' in params.keys():
+            dim2 = params['dim2']
+            if 'data' in locals():
+                data = data.filter(dim2 = dim2)
+            else: 
+                data = Log.objects.filter(dim2 = dim2)
+                
+        #Calculate the required data
+        if method == 'Min':
+            if 'data' in locals():
+                minimum = data.filter(timestamp__range=rng).aggregate(Min('value'))['value__min']
+            else:
+                minimum = Log.objects.filter(timestamp__range=rng).aggregate(Min('value'))['value__min']
+            return Response(minimum)
+        elif method == 'Max':
+            if 'data' in locals():
+                maximum = data.filter(timestamp__range=rng).aggregate(Max('value'))['value__max']
+            else:
+                maximum = Log.objects.filter(timestamp__range=rng).aggregate(Max('value'))['value__max']
+            return Response(maximum)
+        elif method == 'Avg':
+            if 'data' in locals():
+                avg = data.filter(timestamp__range=rng).aggregate(Avg('value'))['value__avg']
+            else:
+                avg = Log.objects.filter(timestamp__range=rng).aggregate(Avg('value'))['value__avg']
+            return Response(avg)
+        elif method == 'StdDev':
+            if 'data' in locals():
+                stddev = data.filter(timestamp__range=rng).aggregate(StdDev('value'))['value__stddev']
+            else:
+                stddev = Log.objects.filter(timestamp__range=rng).aggregate(StdDev('value'))['value__stddev']
+            return Response(stddev)
+        elif method == 'MvgAvg':
+            n = 5
+            if 'n' in params.keys():
+                n = params['n']
+            if 'data' in locals():
+                mvgavg = data.filter(timestamp__range=rng).aggregate(Avg('value'))['value__avg']
+            else:
+                mvgavg = Log.objects.filter(timestamp__range=rng).order_by('-timestamp')[:n].aggregate(Avg('value'))['value__avg']
+            return Response(mvgavg)
+    
 
     def post(self, request, format=None):
         serializer = LogSerializer(data=request.DATA, many = True)
@@ -22,10 +74,8 @@ class LogList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)            
     
+"""
 class LogDetail(APIView):
-    """
-    Retrieve a log instance.
-    """
     def get_object(self, pk):
         try:
             return Log.objects.get(timestamp=pk)
@@ -49,79 +99,11 @@ class LogDetail(APIView):
         log = self.get_object(pk)
         log.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-class LogMax(APIView):
-    """
-    Retrieve Max value between t1 and t2.
-    """
-    def get(self, request, ts1, ts2, dim1 = None, dim2 = None):
-        rng = (ts1,ts2)
-        data = Log.objects.all()
-        if dim1 != None:
-            data = data.filter(dim1 = dim1)
-        if dim2 != None:
-            data = data.filter(dim2 = dim2)
-        data = data.filter(timestamp__range=rng)
-        maximum = data.aggregate(Max('value'))
-        return Response(maximum)
-
-class LogMin(APIView):
-    """
-    Retrieve Min value between t1 and t2.
-    """
-    def get(self, request, ts1, ts2, dim1 = None, dim2 = None):
-        rng = (ts1,ts2)
-        data = Log.objects.all()
-        if dim1 != None:
-            data = data.filter(dim1 = dim1)
-        if dim2 != None:
-            data = data.filter(dim2 = dim2)
-        data = data.filter(timestamp__range=rng)
-        minimum = data.filter(timestamp__range=rng).aggregate(Min('value'))
-        return Response(minimum)
-
-class LogAvg(APIView):
-    """
-    Retrieve Avg value between t1 and t2.
-    """
-    def get(self, request, ts1, ts2, dim1 = None, dim2 = None):
-        rng = (ts1,ts2)
-        data = Log.objects.all()
-        if dim1 != None:
-            data = data.filter(dim1 = dim1)
-        if dim2 != None:
-            data = data.filter(dim2 = dim2)
-        data = data.filter(timestamp__range=rng)
-        avg = data.filter(timestamp__range=rng).aggregate(Avg('value'))
-        return Response(avg)
         
-class LogStdDev(APIView):
-    """
-    Retrieve Standard deviation for values between t1 and t2.
-    """
-    def get(self, request, ts1, ts2, dim1 = None, dim2 = None):
-        rng = (ts1,ts2)
-        data = Log.objects.all()
-        if dim1 != None:
-            data = data.filter(dim1 = dim1)
-        if dim2 != None:
-            data = data.filter(dim2 = dim2)
-        data = data.filter(timestamp__range=rng)
-        stddev = data.objects.filter(timestamp__range=rng).aggregate(StdDev('value'))
-        return Response(stddev)
+class LogList(APIView):
+     def get(self, request, format=None):
+        log = Log.objects.all()
+        serializer = LogSerializer(log, many=True)
+        return Response(serializer.data)
         
-class LogMvgAvg(APIView):
-    """
-    Retrieve the n-element moving average for values between t1 and t2.
-    """
-    def get(self, request, ts1, ts2, n, dim1 = None, dim2 = None):
-        rng = (ts1,ts2)
-        data = Log.objects.all()
-        if dim1 != None:
-            data = data.filter(dim1 = dim1)
-        if dim2 != None:
-            data = data.filter(dim2 = dim2)
-        data = data.filter(timestamp__range=rng)        
-        mvgavg = data.filter(timestamp__range=rng).order_by('-timestamp')[:n].aggregate(Avg('value'))
-        return Response(mvgavg)
-        
+"""
